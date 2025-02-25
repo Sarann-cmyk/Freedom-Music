10f12bb7212a355a220f8dc9173851d543238eae
Alex Synelnyk <xalev@ukr.net>
2025-02-25 11:34:46 +0200

    Initial Commit

---
+++ Freedom Music/Freedom_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Freedom_MusicApp.swift
+//  Freedom Music
+//
+//  Created by Aleks Synelnyk on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Freedom_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
