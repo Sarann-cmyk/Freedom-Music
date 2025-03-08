5a28ecc8523cc6657ab10809714e03f05858bf9e
Alex Synelnyk <xalev@ukr.net>
2025-03-08 11:37:44 +0200

    Implementation of methods: displaying duration, displaying coverImage, playing audio file, creating application icon

---
+++ Freedom Music/Helper/SizeConstant.swift
@@ -0,0 +1,8 @@
+//
+//  SizeConstant.swift
+//  Freedom Music
+//
+//  Created by Aleks Synelnyk on 03.03.2025.
+//
+
+import Foundation
---
+++ Freedom Music/Views/Subviews/SongImageView.swift
@@ -0,0 +1,18 @@
+//
+//  SongImageView.swift
+//  Freedom Music
+//
+//  Created by Aleks Synelnyk on 03.03.2025.
+//
+
+import SwiftUI
+
+struct SongImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongImageView()
+}
